5978d34cb2ea9e4df3c05258d306b951196ff00d
jnakht <jisan.cse.cu@gmail.com>
2025-04-03 13:30:21 +0600

    context created

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './routes/Routes.jsx';
+import AuthProvider from './providers/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <RouterProvider router={router}></RouterProvider>
+      <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
   </StrictMode>,
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Home = () => {
+    const {str} = useContext(AuthContext);
     return (
         <div>
-            this is home
+            this is home {str}
         </div>
     );
 };
